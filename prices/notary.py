8ea233d4c831595555a897725758984317ac8965
Álisson de Moraes Zimermann <alissonzima@gmail.com>
2025-01-14 23:56:42 -0300

    Create notary.py

---
+++ prices/notary.py
@@ -0,0 +1,23 @@
+notarys = [
+    {
+        "id": "notary_1",
+        "nome": "Notary 1",
+        "qtd_autenticacao": "10",
+        "qtd_assinatura": "5",
+        "qtd_template": "1"
+    },
+    {
+        "id": "notary_2",
+        "nome": "Notary 2",
+        "qtd_autenticacao": "20",
+        "qtd_assinatura": "10",
+        "qtd_template": "3"
+    },
+    {
+        "id": "notary_3",
+        "nome": "Notary 3",
+        "qtd_autenticacao": "40",
+        "qtd_assinatura": "20",
+        "qtd_template": "3"
+    }
+]
