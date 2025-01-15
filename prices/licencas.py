1f11fe27656f864bab21d69eacbc70de1b7666a8
Álisson de Moraes Zimermann <alissonzima@gmail.com>
2025-01-14 23:56:26 -0300

    Create licencas.py

---
+++ prices/licencas.py
@@ -0,0 +1,44 @@
+licencas = [
+    {
+        "id": "plano_1",
+        "nome": "Plano 1",
+        "qtd_pc": "1",
+        "qtd_mobile": "1",
+        "qtd_server": "0"
+    },
+    {
+        "id": "plano_2",
+        "nome": "Plano 2",
+        "qtd_pc": "1",
+        "qtd_mobile": "2",
+        "qtd_server": "0"
+    },
+    {
+        "id": "plano_3",
+        "nome": "Plano 3",
+        "qtd_pc": "1",
+        "qtd_mobile": "3",
+        "qtd_server": "0"
+    },
+    {
+        "id": "plano_4",
+        "nome": "Plano 4",
+        "qtd_pc": "2",
+        "qtd_mobile": "3",
+        "qtd_server": "0"
+    },
+    {
+        "id": "plano_5",
+        "nome": "Plano 5",
+        "qtd_pc": "3",
+        "qtd_mobile": "3",
+        "qtd_server": "0"
+    },
+    {
+        "id": "plano_6",
+        "nome": "Plano 6",
+        "qtd_pc": "3",
+        "qtd_mobile": "3",
+        "qtd_server": "1"
+    }
+]
