4f19e85ccfa46c0f86110ee3eb3f273e25f9d82c
Álisson de Moraes Zimermann <alissonzima@gmail.com>
2025-01-14 23:56:59 -0300

    Create planos.py

---
+++ prices/planos.py
@@ -0,0 +1,348 @@
+planos = [
+    {
+        "id": "vol_ba_100",
+        "nome": "Backup Avançado",
+        "volume": "100",
+        "cobranca": "Volume",
+        "valor": "100"
+    },
+    {
+        "id": "vol_ba_200",
+        "nome": "Backup Avançado",
+        "volume": "200",
+        "cobranca": "Volume",
+        "valor": "200"
+    },
+    {
+        "id": "vol_ba_500",
+        "nome": "Backup Avançado",
+        "volume": "500",
+        "cobranca": "Volume",
+        "valor": "500"
+    },
+    {
+        "id": "wkl_ba1_100",
+        "nome": "Backup Avançado 1",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "111"
+    },
+    {
+        "id": "wkl_ba1_200",
+        "nome": "Backup Avançado 1",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "112"
+    },
+    {
+        "id": "wkl_ba1_500",
+        "nome": "Backup Avançado 1",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "115"
+    },
+    {
+        "id": "wkl_ba2_100",
+        "nome": "Backup Avançado 2",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "221"
+    },
+    {
+        "id": "wkl_ba2_200",
+        "nome": "Backup Avançado 2",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "222"
+    },
+    {
+        "id": "wkl_ba2_500",
+        "nome": "Backup Avançado 2",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "225"
+    },
+    {
+        "id": "wkl_ba3_100",
+        "nome": "Backup Avançado 3",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "331"
+    },
+    {
+        "id": "wkl_ba3_200",
+        "nome": "Backup Avançado 3",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "332"
+    },
+    {
+        "id": "wkl_ba3_500",
+        "nome": "Backup Avançado 3",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "335"
+    },
+    {
+        "id": "wkl_ba4_100",
+        "nome": "Backup Avançado 4",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "441"
+    },
+    {
+        "id": "wkl_ba4_200",
+        "nome": "Backup Avançado 4",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "442"
+    },
+    {
+        "id": "wkl_ba4_500",
+        "nome": "Backup Avançado 4",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "445"
+    },
+    {
+        "id": "wkl_ba5_100",
+        "nome": "Backup Avançado 5",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "551"
+    },
+    {
+        "id": "wkl_ba5_200",
+        "nome": "Backup Avançado 5",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "552"
+    },
+    {
+        "id": "wkl_ba5_500",
+        "nome": "Backup Avançado 5",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "555"
+    },
+    {
+        "id": "wkl_ba6_100",
+        "nome": "Backup Avançado 6",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "661"
+    },
+    {
+        "id": "wkl_ba6_200",
+        "nome": "Backup Avançado 6",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "662"
+    },
+    {
+        "id": "wkl_ba6_500",
+        "nome": "Backup Avançado 6",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "665"
+    },
+    {
+        "id": "vol_bad_100",
+        "nome": "Backup de Alta Disponibilidade",
+        "volume": "100",
+        "cobranca": "Volume",
+        "valor": "1000"
+    },
+    {
+        "id": "vol_bad_200",
+        "nome": "Backup de Alta Disponibilidade",
+        "volume": "200",
+        "cobranca": "Volume",
+        "valor": "2000"
+    },
+    {
+        "id": "vol_bad_500",
+        "nome": "Backup de Alta Disponibilidade",
+        "volume": "500",
+        "cobranca": "Volume",
+        "valor": "5000"
+    },
+    {
+        "id": "wkl_bad1_100",
+        "nome": "Backup de Alta Disponibilidade 1",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "1111"
+    },
+    {
+        "id": "wkl_bad1_200",
+        "nome": "Backup de Alta Disponibilidade 1",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "1112"
+    },
+    {
+        "id": "wkl_bad1_500",
+        "nome": "Backup de Alta Disponibilidade 1",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "1115"
+    },
+    {
+        "id": "wkl_bad2_100",
+        "nome": "Backup de Alta Disponibilidade 2",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "2221"
+    },
+    {
+        "id": "wkl_bad2_200",
+        "nome": "Backup de Alta Disponibilidade 2",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "2222"
+    },
+    {
+        "id": "wkl_bad2_500",
+        "nome": "Backup de Alta Disponibilidade 2",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "2225"
+    },
+    {
+        "id": "wkl_bad3_100",
+        "nome": "Backup de Alta Disponibilidade 3",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "3331"
+    },
+    {
+        "id": "wkl_bad3_200",
+        "nome": "Backup de Alta Disponibilidade 3",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "3332"
+    },
+    {
+        "id": "wkl_bad3_500",
+        "nome": "Backup de Alta Disponibilidade 3",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "3335"
+    },
+    {
+        "id": "wkl_bad4_100",
+        "nome": "Backup de Alta Disponibilidade 4",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "4441"
+    },
+    {
+        "id": "wkl_bad4_200",
+        "nome": "Backup de Alta Disponibilidade 4",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "4442"
+    },
+    {
+        "id": "wkl_bad4_500",
+        "nome": "Backup de Alta Disponibilidade 4",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "4445"
+    },
+    {
+        "id": "wkl_bad5_100",
+        "nome": "Backup de Alta Disponibilidade 5",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "5551"
+    },
+    {
+        "id": "wkl_bad5_200",
+        "nome": "Backup de Alta Disponibilidade 5",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "5552"
+    },
+    {
+        "id": "wkl_bad5_500",
+        "nome": "Backup de Alta Disponibilidade 5",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "5555"
+    },
+    {
+        "id": "wkl_bad6_100",
+        "nome": "Backup de Alta Disponibilidade 6",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "6661"
+    },
+    {
+        "id": "wkl_bad6_200",
+        "nome": "Backup de Alta Disponibilidade 6",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "6662"
+    },
+    {
+        "id": "wkl_bad6_500",
+        "nome": "Backup de Alta Disponibilidade 6",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "6665"
+    },
+    {
+        "id": "wkl_vol_aa",
+        "nome": "Antivírus e Antimalware",
+        "valor": "15"
+    },
+    {
+        "id": "vol_fss_100",
+        "nome": "File Sync and Share",
+        "volume": "100",
+        "cobranca": "Volume",
+        "valor": "110"
+    },
+    {
+        "id": "vol_fss_200",
+        "nome": "File Sync and Share",
+        "volume": "200",
+        "cobranca": "Volume",
+        "valor": "220"
+    },
+    {
+        "id": "vol_fss_500",
+        "nome": "File Sync and Share",
+        "volume": "500",
+        "cobranca": "Volume",
+        "valor": "550"
+    },
+    {
+        "id": "wkl_fss_100",
+        "nome": "File Sync and Share",
+        "volume": "100",
+        "cobranca": "Workload",
+        "valor": "60"
+    },
+    {
+        "id": "wkl_fss_200",
+        "nome": "File Sync and Share",
+        "volume": "200",
+        "cobranca": "Workload",
+        "valor": "120"
+    },
+    {
+        "id": "wkl_fss_500",
+        "nome": "File Sync and Share",
+        "volume": "500",
+        "cobranca": "Workload",
+        "valor": "360"
+    },
+    {
+        "id": "wkl_vol_nty",
+        "nome": "Notary",
+        "valor": "30"
+    }
+]
